c6a524bcf2fca198bdfb6e8ce0ae1e86b9ddfbe1
PavelGnatyuk <pgnatyuk@gmail.com>
2013-02-04 09:49:03 +0200

    initialize

---
+++ OnAppStore/OnAppStore/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OnAppStore
+//
+//  Created by Pavel Gnatyuk on 2/4/13.
+//  Copyright (c) 2013 Pavel Gnatyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OnAppStore/OnAppStore/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OnAppStore
+//
+//  Created by Pavel Gnatyuk on 2/4/13.
+//  Copyright (c) 2013 Pavel Gnatyuk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
