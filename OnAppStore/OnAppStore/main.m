fd192c18ea7e7b99487825f42e4860a996dc54b4
PavelGnatyuk <pgnatyuk@gmail.com>
2013-02-04 09:49:14 +0200

    initial

---
+++ OnAppStore/OnAppStore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OnAppStore
+//
+//  Created by Pavel Gnatyuk on 2/4/13.
+//  Copyright (c) 2013 Pavel Gnatyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
